eea1aef7d4de813b54f053376e3029ca3826bb8f
Julie Brunet <juliebrunet21@msn.com>
2023-02-27 15:16:58 +0100

    ajout de la home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @clubs = Club.all
+  end
+
+  def dashboard
+    @clubs = Club.all
+    @bookings = Booking.all
+  end
+
+  def club_params
+    params.require(:clubs).permit(:name, :description, :phone_number, :type, :capacity, :hour_price, :user_id)
   end
 end
