b002eb2bd16093e9e971b520cb5cc5acb88a02b0
Bastien Lafont <lafont.bastien@gmail.com>
2023-02-28 10:48:58 +0100

    delete user_id from strong params because of devise and current_user

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -31,7 +31,7 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :accepted, :user_id, :club_id)
+    params.require(:booking).permit(:start_date, :end_date, :accepted, :club_id)
   end
 
   def set_club
