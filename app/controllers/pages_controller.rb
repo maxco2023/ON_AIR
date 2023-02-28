1233831360e6f5d7a1e0caa7b74c9083044e9bd8
Julie Brunet <juliebrunet21@msn.com>
2023-02-28 09:47:59 +0100

    home and dasboard ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
 
   def dashboard
     # pas besoin de la première ligne ? mettre plutôt juste @club.user = current_user ?
-    @clubs = Club.where(:user, current_user)
-    @bookings = Booking.where(:user, current_user)
+    @clubs = Club.where(user: current_user)
+    @bookings = Booking.where(user: current_user)
   end
 end
