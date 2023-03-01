7d45635c5a86a11447b85f95a953f2e0711e4ae6
maxco2023 <maxime.coquerelle@gmail.com>
2023-03-01 10:57:32 +0100

    validates start and end date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :club
   belongs_to :user
 
-  validates :start_date, presence: true, comparison: { less_than: :end_date }
-  validates :end_date, presence: true, comparison: { greater_than: :start_date }
+  validates :start_date, presence: true, comparison: { less_than_or_equal_to: :end_date }
+  validates :end_date, presence: true, comparison: { greater_than_or_equal_to: :start_date }
+
 
   def start_time
     self.start_date ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
