30e7dffc2a5ab29a807fd33cd307c44d27a5d7bb
maxco2023 <maxime.coquerelle@gmail.com>
2023-03-02 13:55:20 +0100

    dashboard onglet ok preview

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,13 +2,16 @@ class Booking < ApplicationRecord
   belongs_to :club
   belongs_to :user
 
+  validates :start_date, uniqueness: true
+  validates :end_date, uniqueness: true
+
   validates :start_date, presence: true, comparison: { less_than_or_equal_to: :end_date }
   validates :end_date, presence: true, comparison: { greater_than_or_equal_to: :start_date }
   validate :start_date_cannot_be_in_the_past
 
   def start_date_cannot_be_in_the_past
     if start_date.present? && start_date < Date.today
-      errors.add(:start_date, "can't be in the past")
+      errors.add(:start_date, "La date de réservation ne peut pas être antérieure à aujourd'hui")
     end
   end
 
