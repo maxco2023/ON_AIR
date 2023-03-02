86f0fc5ac18c403f54ed9d5a299e30ff22076dcc
Bastien Lafont <lafont.bastien@gmail.com>
2023-03-02 11:11:18 +0100

    corrected seed+contents

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,7 +36,7 @@ club2 = Club.new(
   description: "Le Paname Comedy Club vous donne rendez-vous pour une heure de stand up avec le meilleur des one man show des humoristes du moment. Ils sont drôles, incisifs, hilarants, redoutables ou tout simplement charmants. Le Paname Comedy Club c'est une heure de fous rires garantie !",
   phone_number: "06 38 43 70 87",
   capacity: 20,
-  category: "restau",
+  category: "Comedy Club",
   hour_price: 18,
   user_id: user2.id
 )
@@ -77,3 +77,86 @@ booking2 = Booking.new(
   user_id: user4.id
 )
 booking2.save!
+
+
+userl = User.new(
+  first_name: "Georges",
+  last_name: "Moustaki",
+  phone_number: "07 25 46 83 82",
+  biography: "Même si je parle encore le français avec un accent de touriste allemand, être un étranger n'est plus une excuse. Je dois maintenant faire face à toutes les problématiques d'un quarantenaire franco-juif-new-yorkais pacsé avec 3 enfants en bas âge. Comment réagir quand ta fille corrige ton français, quand ta nana reçoit une banane en pleine gueule, quand ta femme de ménage trouve ta maison dégueulasse, quand t'es une cigale perdue dans un monde de fourmis ? Les choses de la vie...",
+  email: "georgesm@lewagon.org",
+  password: "123456"
+)
+userl.save!
+
+clubl1 = Club.new(
+  name: "Le Fridge Comedy",
+  address: "164 Rue Saint-Denis, 75002 Paris",
+  description: "Restaurant intimiste et comedy club accueillant des one-man-show en soirée et proposant des repas légers, du vin et des cocktails.",
+  phone_number: "06 21 34 51 01",
+  capacity: 50,
+  category: "Restaurant",
+  hour_price: 20,
+  user_id: userl.id
+)
+clubl1.save!
+
+clubl2 = Club.new(
+  name: "Le Jamel Comedy Club",
+  address: "42 Bd de Bonne Nouvelle, 75010 Paris",
+  description: "Un bar-lounge et une scène accueillant de jeunes talents pour des spectacles d'humour et des jam sessions.",
+  phone_number: "06 32 44 52 11",
+  capacity: 60,
+  category: "Comedy Club",
+  hour_price: 18,
+  user_id: userl.id
+)
+clubl2.save!
+
+clubl3 = Club.new(
+  name: "La Scène Barbès",
+  address: "11 Rue d'Oran, 75018 Paris",
+  description: "La Scène Barbès est une salle de spectacles principalement axée sur le stand-up. Vous pourrez aussi y découvrir des scènes musicales le week-end. La programmation du lieu est à découvrir sur notre site. Dans un décor et une ambiance inspirée des comedy club New-Yorkais, vous découvrirez 5 humoristes qui se succèderont pour une heure de show pour le plateau des artistes confirmés.",
+  phone_number: "01 42 51 14 80",
+  capacity: 30,
+  category: "Comedy Club",
+  hour_price: 14,
+  user_id: userl.id
+)
+clubl3.save!
+
+clubl4 = Club.new(
+  name: "Café Oscar",
+  address: "155 Rue Montmartre, 75002 Paris",
+  description: "Chaque soir, les meilleurs humoristes parisiens dans l'atmosphère intimiste du mythique Café Oscar !",
+  phone_number: "01 42 21 09 61",
+  capacity: 20,
+  category: "Café",
+  hour_price: 12,
+  user_id: userl.id
+)
+clubl4.save!
+
+clubl5 = Club.new(
+  name: "The Junk",
+  address: "37 R. Quincampoix, 75004 Paris",
+  description: "The Joke est un concept qui a été pensé par des amoureux du Stand up. Du bar jusqu’à la salle de spectacle au fond, nous combinons un environnement sobre et chic parsemé d’une dose d’humour pour vous donner la meilleure expérience possible. Rien de mieux qu’un bon verre avec un spectacle",
+  phone_number: "01 88 40 03 40",
+  capacity: 30,
+  category: "Comedy Club",
+  hour_price: 15,
+  user_id: userl.id
+)
+clubl5.save!
+
+clubl6 = Club.new(
+  name: "Le Loup Club",
+  address: "7 Rue de l'Échelle, 75001 Paris",
+  description: "Le Loup Comedy et sa meute d’humoristes vous offre un spectacle rythmé par les punchlines. C’est l’occasion de voir les grosses pointures et nouvelles pépites de l'humour francophone!",
+  phone_number: "01 32 69 84 39",
+  capacity: 40,
+  category: "Comedy Club",
+  hour_price: 16,
+  user_id: userl.id
+)
+clubl6.save!
